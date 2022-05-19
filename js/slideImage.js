4c16282a4f920d42365dd252d79bf676356f8fdb
Swapnil Dharawat <sdharawatnet@gmail.com>
2022-05-19 09:47:05 -0400

    initial commit

---
+++ js/slideImage.js
@@ -0,0 +1,8 @@
+function imgSlide(anything){
+    document.querySelector('.ps5_controller').src = anything;
+}
+
+function changeCircleColor(color){
+    const circle = document.querySelector('.containDesign')
+    circle.style.background = color;
+}
